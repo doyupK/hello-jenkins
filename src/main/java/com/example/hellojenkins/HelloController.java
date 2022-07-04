00a4d3c190e4637b38d536ccfe7bfed4476cbcc2
DoyupK <gltlvl12@gmail.com>
2022-07-04 22:36:58 +0900

    why..aaaabbbb

--- src/main/java/com/example/hellojenkins/HelloController.java
+++ src/main/java/com/example/hellojenkins/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String sayHello() {
-        return "hell, what why why!!@#!@#!!";
+        return "hell, what why why!!T.T";
     }
 }
