3f66bd9966ca2dbe6cd0ca9c978fbb9d4695c6e0
DoyupK <gltlvl12@gmail.com>
2022-07-04 16:09:27 +0900

    test1

--- src/main/java/com/example/hellojenkins/HelloController.java
+++ src/main/java/com/example/hellojenkins/HelloController.java
@@ -1,2 +1,12 @@
-package com.example.hellojenkins;public class HelloController {
+package com.example.hellojenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping
+    public String sayHello(){
+        return "hell, jenkins";
+    }
 }
