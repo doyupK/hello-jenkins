af780549ee29075434a1f168cd39fa21ed4886e3
DoyupK <gltlvl12@gmail.com>
2022-07-05 17:27:14 +0900

    1234

--- src/main/java/com/example/hellojenkins/HelloController.java
+++ src/main/java/com/example/hellojenkins/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String sayHello() {
-        return "123";
+        return "1234";
     }
 }
